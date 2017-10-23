652822213528c077456d72492ed05000162205f8
bas080 <bas080@hotmail.com>
2017-10-23 15:16:57 +0200

    Support both bash and zsh
    Have separate files that are sourced for both zsh and bash
    
    Common function are stored in projector.sh
    
    Also updates the install script to correctly set this up

--- install.sh
+++ install.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
-SCRIPT_URL="https://raw.githubusercontent.com/kalouantonis/projector/master/projector.sh"
-SCRIPT_PATH="$HOME/.projector"
-SOURCE_COMMAND="source ~/.projector"
-SOURCE_SCRIPT="$HOME/.bash_profile"
+PROJECTOR_URL="https://raw.githubusercontent.com/kalouantonis/projector/master/projector.sh"
+PROJECTOR_PATH="$HOME/.projector"
 
-curl "$SCRIPT_URL" > "$SCRIPT_PATH" && \
-  grep "$SOURCE_COMMAND" "$SOURCE_SCRIPT" || \
-  echo "$SOURCE_COMMAND" >> "$SOURCE_SCRIPT"
+PROJECTOR_BASH_URL="https://raw.githubusercontent.com/kalouantonis/projector/master/projector_bash.sh"
+PROJECTOR_BASH_PATH="$HOME/.projector_bash"
+
+PROJECTOR_ZSH_URL="https://raw.githubusercontent.com/kalouantonis/projector/master/projector_zsh.sh"
+PROJECTOR_ZSH_PATH="$HOME/.projector_zsh"
+
+function install() {
+  download "$PROJECTOR_ZSH_URL" "$PROJECTOR_ZSH_PATH" && \
+    download "$PROJECTOR_BASH_URL" "$PROJECTOR_BASH_PATH" && \
+    download "$PROJECTOR_URL" "$PROJECTOR_PATH" && \
+    setup_env "$HOME/.bashrc" "source ~/.projector_bash" && \
+    setup_env "$HOME/.zshrc"  "source ~/.projector_zsh"
+}
+
+function download() {
+  local url="$1"
+  local filename="$2"
+
+  curl -f "$url" > "$filename"
+}
+
+function setup_env() {
+  local source_file="$1"
+  local source_command="$2"
+
+  test ! -f $source_file && return 0
+
+  grep -q "$source_command" "$source_file" || \
+    echo "$source_command" >> "$source_file"
+}
+
+install $@ && echo succes || echo failure
