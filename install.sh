c65515a4700f4d6c9b0ddc16c0912fe5813857c9
bas080 <bas080@hotmail.com>
2017-10-23 15:28:09 +0200

    Simplify installation and updating
    Have the install script download the projector script and only add the source
    script if it hasn't been added already

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
-cp projector.sh ~/.projector
-echo "source ~/.projector" >> ~/.bashrc
+SCRIPT_URL="https://raw.githubusercontent.com/kalouantonis/projector/master/projector.sh"
+SCRIPT_PATH="$HOME/.projector"
+SOURCE_COMMAND="source ~/.projector"
+SOURCE_SCRIPT="$HOME/.bash_profile"
+
+curl "$SCRIPT_URL" > "$SCRIPT_PATH" && \
+  grep "$SOURCE_COMMAND" "$SOURCE_SCRIPT" || \
+  echo "$SOURCE_COMMAND" >> "$SOURCE_SCRIPT"
