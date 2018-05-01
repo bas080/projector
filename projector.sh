98ba1e4972ecce3ec551e189548a1f91ee3f8a52
Bas Huis <bas080@hotmail.com>
2018-04-30 22:38:06 -0400

    Make re-source when bash_project is modified
    Also notify user when (re)sourced

--- projector.sh
+++ projector.sh
@@ -2,20 +2,40 @@ PROJECTOR_SOURCE_PATH='.bash_project'
 PROJECTOR_BIN_PATH='projector_bin'
 
 projector_prompt() {
-  local projector_path=$(projector_path_find $PWD)
+  local projector_path="$(projector_path_find $PWD)"
 
-  # do nothing if the project path has not changed
-  test "$projector_path" = "$PROJECT_HOME" && return 1
+  test -z $projector_path && return 1
 
-  PROJECT_HOME="$projector_path"
+  projector_path_changed "$projector_path" || \
+    projector_source_modified "$projector_path" && \
+    projector_source_bash_project "$projector_path"
 
-  source_bash_project "$projector_path"
   set_path_with_projector_path "$projector_path"
 }
 
-source_bash_project() {
-  test -f "$1/$PROJECTOR_SOURCE_PATH" && \
-    source "$1/$PROJECTOR_SOURCE_PATH"
+projector_path_changed() {
+  test "$1" != "$PROJECT_HOME"
+}
+
+projector_source_modified() {
+  test "$PROJECTOR_SOURCE_MOD_DATE" != "$(projector_bash_project_mod_date $1)"
+}
+
+projector_bash_project_mod_date() {
+  stat -c %y "$1/$PROJECTOR_SOURCE_PATH"
+}
+
+projector_source_bash_project() {
+  local file="$1/$PROJECTOR_SOURCE_PATH"
+  PROJECT_HOME="$1"
+  PROJECTOR_SOURCE_MOD_DATE="$(projector_bash_project_mod_date $1)"
+
+  test -f "$file" && \
+    source "$file" && projector_log "sourced $file"
+}
+
+projector_log() {
+  echo "projector: $@"
 }
 
 set_path_with_projector_path() {
@@ -35,7 +55,6 @@ projector_is_project_dir() {
 }
 
 projector_path_find() {
-  # find the nearest projector parent directory
   projector_is_project_dir "$1" && echo "$1" && return 0
   test '/' = "$1" && return 1
 
